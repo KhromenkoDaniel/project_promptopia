167356973a7cbe6cefbcc611c73bed8c84ef73ac
KhromenkoDaniel <daniilkhro.study@gmail.com>
2024-01-24 20:56:54 +0200

    fix boundary with useSearchParams

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        appDir: true,
         serverComponentsExternalPackages: ["mongoose"],
+        missingSuspenseWithCSRBailout: false,
     },
     images: {
         domains: ['lh3.googleusercontent.com'],
@@ -11,9 +11,9 @@ const nextConfig = {
         config.experiments = {
             ...config.experiments,
             topLevelAwait: true,
-        }
-        return config
-    }
-}
+        };
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
